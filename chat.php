f3c783201b80ec7285d716c0a348bc8fb5882dca
HILITEGS <74969363+HILITEGS@users.noreply.github.com>
2020-11-26 19:32:33 +0400

    Update chat.php

--- chat.php
+++ chat.php
@@ -1,5 +1,5 @@
 <?
 $text = urldecode($_get['text']);/// загружает инфо из программы
-file_put_contents('chat.txt', $text, FILE_APPEND); ///ЗАГРУЖАЕТ В ФАЙЛ ИЗ ПРОГРАММЫ.
+file_put_contents('ghjghhjk.txt', $text, FILE_APPEND); ///ЗАГРУЖАЕТ В ФАЙЛ ИЗ ПРОГРАММЫ.
 ?>
 
